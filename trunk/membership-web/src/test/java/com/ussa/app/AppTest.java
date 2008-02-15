f490ca06a9266fcdb978d6f72e4d81de1b147872
bweston <bweston@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-02-15 16:06:18 +0000

    deleted because the project is corrupt

--- trunk/membership-web/src/test/java/com/ussa/app/AppTest.java
+++
@@ -1,9 +0,0 @@
-package com.ussa.app;
-
-import junit.framework.TestCase;
-
-public class AppTest extends TestCase {
-    public void testGetHello() throws Exception {
-        assertEquals("Hello", App.getHello());
-    }
-}
