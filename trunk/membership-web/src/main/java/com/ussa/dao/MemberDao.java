32118da0f8d82ca5922d8fc7e326864b7f1bb196
bweston <bweston@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-02-16 06:22:05 +0000

    added dao and managers for membership application

---
+++ trunk/membership-web/src/main/java/com/ussa/dao/MemberDao.java
@@ -0,0 +1,10 @@
+package com.ussa.dao;
+
+import org.appfuse.dao.GenericDao;
+import com.ussa.model.Member;
+
+public interface MemberDao extends GenericDao<Member, Long>
+{
+
+
+}
---
+++ trunk/membership-web/src/main/java/com/ussa/manager/MemberManager.java
@@ -0,0 +1,11 @@
+package com.ussa.manager;
+
+import org.appfuse.service.GenericManager;
+import com.ussa.model.Member;
+import java.util.List;
+
+public interface MemberManager extends GenericManager<Member, Long>
+{
+
+}
+
