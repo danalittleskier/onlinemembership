1e14973a04b581fb787133b27fbaa5aa650564bc
bweston <bweston@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-02-19 18:46:01 +0000

    added abstract controllers add util and java files added test code to contact controller and jsp

--- trunk/membership-web/src/main/java/com/ussa/spring/controller/ContactController.java
+++ trunk/membership-web/src/main/java/com/ussa/spring/controller/ContactController.java
@@ -1,23 +1,91 @@
 package com.ussa.spring.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import com.ussa.spring.controller.AbstractSimpleFormController;
 
-public class ContactController extends AbstractController
+import com.ussa.dao.MemberDao;
+import com.ussa.manager.MemberManager;
+import com.ussa.model.Member;
+
+public class ContactController extends AbstractSimpleFormController
 {
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-            throws Exception
+
+    /** Logger for this class and subclasses */
+    protected final Log log = LogFactory.getLog(getClass());
+
+    private MemberDao memberDao;
+    private MemberManager memberManager;
+    public ContactController()
     {
-        System.out.println("Hello from Contact Controller");
-        Exception exception = (Exception) request.getAttribute("javax.servlet.error.exception");
-        if (exception != null)
-        {
-            throw exception;
-        }
-        return new ModelAndView("Contact");
+        setCommandName("member");
+        setCommandClass(Member.class);
+    }
+    protected Map referenceData(HttpServletRequest request) throws Exception
+    {
+        Map model = new HashMap();
+        //model.put("selected", selected);
+        return model;
+    }
+
+    @Override
+    protected ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response, Object command,
+            BindException errors) throws Exception
+    {
+        ModelAndView result = null;
+        Member obj = (Member) command;
+        result = new ModelAndView(getSuccessView());
+        return result;
+    }
+
+
+    protected Object formBackingObject(HttpServletRequest request)
+            throws Exception
+            {
+                Member obj = new Member();
+                Long ussaId = ServletRequestUtils.getLongParameter(request, "ussaId", 0);
+
+
+
+
+         return obj;
+            }
+
+    /**
+     * @return the memberDao
+     */
+    public MemberDao getMemberDao()
+    {
+        return memberDao;
+    }
+    /**
+     * @param memberDao the memberDao to set
+     */
+    public void setMemberDao(MemberDao memberDao)
+    {
+        this.memberDao = memberDao;
+    }
+    /**
+     * @return the memberManager
+     */
+    public MemberManager getMemberManager()
+    {
+        return memberManager;
+    }
+    /**
+     * @param memberManager the memberManager to set
+     */
+    public void setMemberManager(MemberManager memberManager)
+    {
+        this.memberManager = memberManager;
     }
 
 }
--- trunk/membership-web/src/main/webapp/WEB-INF/pages/contact.jsp
+++ trunk/membership-web/src/main/webapp/WEB-INF/pages/contact.jsp
@@ -1,1 +1,68 @@
-contact
+<%@ include file="/common/taglibs.jsp"%>
+<h2><b>CONTACT PAGE</b></h2>
+<br/>
+
+ <form:form commandName="member" name="member">
+   <table class="formtable">
+	<tbody>
+		<spring:bind path="member.*">
+			<c:if test="${not empty status.errorMessages}">
+				<c:set var="sectionHasFormErrors" scope="request" value="true"/>
+				<tr>
+					<td colspan="3" class="formerrors">
+						<c:forEach var="errorMsg" items="${status.errorMessages}">
+							<c:out value="${errorMsg}" escapeXml="false" />
+						</c:forEach>
+					</td>
+				</tr>
+			</c:if>
+		</spring:bind>
+
+	    <tr><td>&nbsp;</td><td colspan="2">&nbsp;</td></tr>
+	    <tr>
+	      <td class="label" colspan="1">First:</td>
+	      <td colspan="2">
+	        <form:input path="FIRST_NAME" size="30" maxlength="30"/>
+	      </td>
+	    </tr>
+	    <tr>
+	      <td class="label" colspan="1">Last:</td>
+	      <td colspan="2">
+	        <form:input path="LAST_NAME" size="30" maxlength="30"/>
+	      </td>
+	    </tr>
+	    <tr><td>&nbsp;</td><td colspan="2">&nbsp;</td></tr>
+	    <tr>
+	      <td class="label" colspan="1">Birthdate:</td>
+	      <td colspan="2">
+	        <form:input path="BIRTHDATE" size="30" maxlength="30"/>
+	      </td>
+	    </tr>
+	    <tr>
+	      <td class="label" colspan="1">Email:</td>
+	      <td colspan="2">
+	        <form:input path="EMAIL" size="30" maxlength="30"/>
+	      </td>
+	    </tr>
+       <tr>
+         <td><br/></td>
+         <td><br/></td>
+       </tr>
+       <tr>
+         <td align="left"></td>
+         <td><br/></td>
+       </tr>
+       <tr>
+         <td><br/></td>
+         <td><br/></td>
+       </tr>
+       <tr>
+         <td><br/></td>
+         <td>
+
+	         <input type="submit" value="<fmt:message key='button.save' />" />&nbsp;
+         <td>
+       </tr>
+    </tbody>
+  </table>
+ </form:form>
