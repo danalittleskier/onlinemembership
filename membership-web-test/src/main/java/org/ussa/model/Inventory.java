739bae2ba0074a63d68c17771e6cd74e825925e9
bweston <bweston@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-02-26 14:25:22 +0000

    upload membership-web-test bundle add your deployment pom for custom deployment

--- membership-web-test/src/main/java/org/ussa/model/Inventory.java
+++ membership-web-test/src/main/java/org/ussa/model/Inventory.java
@@ -13,8 +13,8 @@ public class Inventory extends BaseObject implements Serializable
     @Column(name = "INV_ID", length = 8)
     private String id;
 
-    @Column(name = "RENEW_DESC", nullable = true, length = 50, unique = false)
-    private String renewDesc;
+    @Column(name = "AMOUNT", nullable = true, length = 5, unique = false)
+    private String amount;
 
     @Column(name = "DESCRIPTION", nullable = true, length = 50, unique = false)
     private String description;
@@ -27,6 +27,12 @@ public class Inventory extends BaseObject implements Serializable
 
     @Column(name = "SPORT_CODE", nullable = true, length = 3, unique = false)
     private String sportCode;
+
+    @Column(name = "INVENTORY_TYPE", nullable = true, length = 20, unique = false)
+    private String inventoryType;
+
+    @Column(name = "ACTIVE", nullable = true, length = 1, unique = false)
+    private String active;
 
     @Override
     public boolean equals(Object o)
@@ -49,64 +55,134 @@ public class Inventory extends BaseObject implements Serializable
         return null;
     }
 
+    /**
+     * @return the id
+     */
     public String getId()
     {
         return id;
     }
 
+    /**
+     * @param id the id to set
+     */
     public void setId(String id)
     {
         this.id = id;
     }
 
-    public String getRenewDesc()
+    /**
+     * @return the amount
+     */
+    public String getAmount()
     {
-        return renewDesc;
+        return amount;
     }
 
-    public void setRenewDesc(String renewDesc)
+    /**
+     * @param amount the amount to set
+     */
+    public void setAmount(String amount)
     {
-        this.renewDesc = renewDesc;
+        this.amount = amount;
     }
 
+    /**
+     * @return the description
+     */
     public String getDescription()
     {
         return description;
     }
 
+    /**
+     * @param description the description to set
+     */
     public void setDescription(String description)
     {
         this.description = description;
     }
 
+    /**
+     * @return the ageFrom
+     */
     public Long getAgeFrom()
     {
         return ageFrom;
     }
 
+    /**
+     * @param ageFrom the ageFrom to set
+     */
     public void setAgeFrom(Long ageFrom)
     {
         this.ageFrom = ageFrom;
     }
 
+    /**
+     * @return the ageTo
+     */
     public Long getAgeTo()
     {
         return ageTo;
     }
 
+    /**
+     * @param ageTo the ageTo to set
+     */
     public void setAgeTo(Long ageTo)
     {
         this.ageTo = ageTo;
     }
 
+    /**
+     * @return the sportCode
+     */
     public String getSportCode()
     {
         return sportCode;
     }
 
+    /**
+     * @param sportCode the sportCode to set
+     */
     public void setSportCode(String sportCode)
     {
         this.sportCode = sportCode;
     }
+
+    /**
+     * @return the inventoryType
+     */
+    public String getInventoryType()
+    {
+        return inventoryType;
+    }
+
+    /**
+     * @param inventoryType the inventoryType to set
+     */
+    public void setInventoryType(String inventoryType)
+    {
+        this.inventoryType = inventoryType;
+    }
+
+    /**
+     * @return the active
+     */
+    public String getActive()
+    {
+        return active;
+    }
+
+    /**
+     * @param active the active to set
+     */
+    public void setActive(String active)
+    {
+        this.active = active;
+    }
+
+
 
 }
