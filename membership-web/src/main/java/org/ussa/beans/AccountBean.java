89d5e3154715ce7042cbd1ec4cdd333ed1503036
mweber <mweber@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-02-26 01:02:48 +0000

    -Got flow more sophisticted (good binding, better dao integration, etc.) -Hibernate/Dao changes to pull more data from existing MPSTEST.

--- membership-web/src/main/java/org/ussa/beans/AccountBean.java
+++ membership-web/src/main/java/org/ussa/beans/AccountBean.java
@@ -1,17 +1,32 @@
 package org.ussa.beans;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import org.ussa.model.Address;
+import org.ussa.model.Club;
 import org.ussa.model.Member;
+import org.ussa.model.State;
 
 public class AccountBean
 {
+    //Fields to save when done
     private Member member;
 
     private Address address;
 
-    private Boolean waiverAgree = false;
+    private Club club;
+
+    //Helper data, not necessarailly persisited...
+    private Boolean waiverAgree = false; //This one will go away
+    private Long clubId = new Long(0);
+    private String stateAffiliation;
+
+
+    //Reference Data fields.  Do not save when done:
+    List<State> usStates;
+    List<Club>  clubsForState;
+
 
     public Member getMember()
     {
@@ -41,6 +56,56 @@ public class AccountBean
     public void setWaiverAgree(Boolean waiverAgree)
     {
         this.waiverAgree = waiverAgree;
+    }
+
+    public List<State> getUsStates()
+    {
+        return usStates;
+    }
+
+    public void setUsStates(List<State> usStates)
+    {
+        this.usStates = usStates;
+    }
+
+    public Club getClub()
+    {
+        return club;
+    }
+
+    public void setClub(Club club)
+    {
+        this.club = club;
+    }
+
+    public String getStateAffiliation()
+    {
+        return stateAffiliation;
+    }
+
+    public void setStateAffiliation(String stateAffiliation)
+    {
+        this.stateAffiliation = stateAffiliation;
+    }
+
+    public List<Club> getClubsForState()
+    {
+        return clubsForState;
+    }
+
+    public void setClubsForState(List<Club> clubsForState)
+    {
+        this.clubsForState = clubsForState;
+    }
+
+    public Long getClubId()
+    {
+        return clubId;
+    }
+
+    public void setClubId(Long clubId)
+    {
+        this.clubId = clubId;
     }
 
 
--- membership-web/src/main/java/org/ussa/spring/flows/registration/RegistrationAction.java
+++ membership-web/src/main/java/org/ussa/spring/flows/registration/RegistrationAction.java
@@ -1,25 +1,35 @@
 package org.ussa.spring.flows.registration;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import org.springframework.web.bind.ServletRequestUtils;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.webflow.action.MultiAction;
-
+import org.springframework.webflow.context.servlet.ServletExternalContext;
+import org.springframework.webflow.core.collection.MutableAttributeMap;
+import org.springframework.webflow.execution.Event;
+import org.springframework.webflow.execution.RequestContext;
 import org.ussa.beans.AccountBean;
 import org.ussa.dao.AddressDao;
+import org.ussa.dao.ClubDao;
 import org.ussa.dao.MemberDao;
+import org.ussa.dao.StateDao;
 import org.ussa.manager.MemberManager;
 import org.ussa.model.Address;
-import org.ussa.model.Member;import org.springframework.webflow.execution.Event;
-import org.springframework.webflow.execution.RequestContext;
-import org.springframework.webflow.context.servlet.ServletExternalContext;
-import javax.servlet.http.HttpServletRequest;
+import org.ussa.model.AddressPk;
+import org.ussa.model.Club;
+import org.ussa.model.Member;
+import org.ussa.model.State;
 
 public class RegistrationAction extends MultiAction implements Serializable
 {
     private MemberManager memberManager;
     private MemberDao memberDao;
     private AddressDao addressDao;
+    private StateDao stateDao;
+    private ClubDao clubDao;
 
 
     public void setMemberManager(MemberManager memberManager)
@@ -49,6 +59,12 @@ public class RegistrationAction extends MultiAction implements Serializable
         Member member = new Member();
         Address address = new Address();
 
+        List<State> usStates = stateDao.getAllStateUS_CodeOrdered();
+        obj.setUsStates(usStates);
+        //System.out.println("NumStates["+usStates.size()+"]");
+        //MutableAttributeMap requestScope = context.getRequestScope();
+        //request.setAttribute("usStates", usStates);
+
         if (id != null)
         {
             //Member exists, do a renew and pre-populate info
@@ -59,7 +75,10 @@ public class RegistrationAction extends MultiAction implements Serializable
                 member = new Member();
             }
 
-            address = addressDao.get(id);
+            AddressPk addressPk = new AddressPk();
+            addressPk.setType("P");
+            addressPk.setId(id);//new Long(6050553)); //Micahel Henderson, lucky john
+            address = addressDao.get(addressPk);
             if (address == null)
             {
                 address = new Address();
@@ -75,6 +94,54 @@ public class RegistrationAction extends MultiAction implements Serializable
             obj.setMember(member);
             return result("register");
         }
+    }
+
+    public Event findClubInfo(RequestContext context) throws Exception
+    {
+        HttpServletRequest request = ((ServletExternalContext)context.getExternalContext()).getRequest();
+        AccountBean obj = (AccountBean) context.getFlowScope().get("accountBean");
+        List<Club> clubs = new ArrayList<Club>();
+        String stateAffiliation = new String();
+        System.out.println("Address's city["+obj.getAddress().getCity()+"]");
+        System.out.println("Address's state["+obj.getAddress().getStateCode()+"]");
+        System.out.println("StateAffiliation["+obj.getStateAffiliation()+"]");
+
+        if (obj != null)
+        {
+            System.out.println("Club Info -- Orig state["+obj.getStateAffiliation()+"]");
+            if (obj.getStateAffiliation() != null)
+            { //If I change the state affiliation (in stateClub) set it here
+                stateAffiliation = obj.getStateAffiliation();
+            }
+            else if (obj.getAddress() != null)
+            { //Coming through first pass regular flow
+                if (obj.getAddress().getStateCode() != null)
+                { //This should always be hit as the user is required to put in an address on the prior screen
+                    stateAffiliation = obj.getAddress().getStateCode();
+
+                }
+            }
+            if (stateAffiliation.length()!=0)
+            {
+                System.out.println("state["+stateAffiliation+"]");
+                clubs = clubDao.findByStateCode(stateAffiliation);
+                System.out.println("clubs["+clubs.size()+"]");
+            }
+        }
+        obj.setClubsForState(clubs);
+        //System.out.println("state["+stateAffiliation+"]");
+        obj.setStateAffiliation(stateAffiliation);
+        obj.getMember().setStateCode(stateAffiliation);//TODO: get rid of stateAffiliation should just use member.stateCode
+        return result("form");
+    }
+
+    public void setStateDao(StateDao stateDao)
+    {
+        this.stateDao = stateDao;
+    }
+    public void setClubDao(ClubDao clubDao)
+    {
+        this.clubDao = clubDao;
     }
 
 }
