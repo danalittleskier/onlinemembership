c83f3bc0dc4cecd570d32191a7ae26a7ebbfcf9a
jjenson <jjenson@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-04-02 23:16:39 +0000

    finished the integrating crowd

--- trunk/membership-web/src/main/java/org/ussa/beans/PaymentBean.java
+++ trunk/membership-web/src/main/java/org/ussa/beans/PaymentBean.java
@@ -2,12 +2,6 @@ package org.ussa.beans;
 
 public class PaymentBean
 {
-	private String firstName;
-	private String lastName;
-	private String address;
-	private String city;
-	private String state;
-	private String zip;
 	private String amount;
 	private String cardNumber;
 	private String expireMonth;
@@ -15,66 +9,6 @@ public class PaymentBean
 	private String securityCode;
 
 	private String completedTransactionId;
-
-	public String getFirstName()
-	{
-		return firstName;
-	}
-
-	public void setFirstName(String firstName)
-	{
-		this.firstName = firstName;
-	}
-
-	public String getLastName()
-	{
-		return lastName;
-	}
-
-	public void setLastName(String lastName)
-	{
-		this.lastName = lastName;
-	}
-
-	public String getAddress()
-	{
-		return address;
-	}
-
-	public void setAddress(String address)
-	{
-		this.address = address;
-	}
-
-	public String getCity()
-	{
-		return city;
-	}
-
-	public void setCity(String city)
-	{
-		this.city = city;
-	}
-
-	public String getState()
-	{
-		return state;
-	}
-
-	public void setState(String state)
-	{
-		this.state = state;
-	}
-
-	public String getZip()
-	{
-		return zip;
-	}
-
-	public void setZip(String zip)
-	{
-		this.zip = zip;
-	}
 
 	public String getAmount()
 	{
