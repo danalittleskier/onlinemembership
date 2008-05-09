4680dd8e4161aff23bc44f980e8894a4df24ecd6
tmacduff <tmacduff@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-05-09 19:53:06 +0000

    MEMWEB-182 : add club affiliation

--- trunk/membership-web/src/main/java/org/ussa/dao/ClubDao.java
+++ trunk/membership-web/src/main/java/org/ussa/dao/ClubDao.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.ussa.common.dao.GenericDao;
 import org.ussa.model.Club;
+import org.ussa.model.Member;
 
 public interface ClubDao extends GenericDao<Club, Long>
 {
 	public List<Club> findByStateCode(String stateCode);
 
 	public Club getClub(Long id);
+	
+	public List<Club> findByMember(Member member);
 }
