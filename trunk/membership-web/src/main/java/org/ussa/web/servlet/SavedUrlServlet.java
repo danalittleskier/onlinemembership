5e0b4c611787aa53fbd3f6073e763b09e743998e
tmacduff <tmacduff@cb3c6908-3955-1147-92f4-2d4536eed4be>
2008-05-12 20:40:51 +0000

    MWEB-218 : refactor name of filter and servelet to be "SavedUrl[Servlet | Filter].

--- trunk/membership-web/src/main/java/org/ussa/web/servlet/SavedUrlServlet.java
+++ trunk/membership-web/src/main/java/org/ussa/web/servlet/SavedUrlServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringUtils;
-import org.ussa.web.filter.SessionMonitorFilter;
+import org.ussa.web.filter.SavedUrlFilter;
 
 /**
  * Per MEMWEB-218 this servlet exists to redirect the url in a way that the Acegi AuthenticationProcessingFilter is supposed to
@@ -16,24 +16,24 @@ import org.ussa.web.filter.SessionMonitorFilter;
  * implementation that the defaultTargetUrl is ignored or overwritten.  Since the source for crowd is not available, this can 
  * neither be confirmed nor denied and is the reason for this servlet / filter work around.  See org.ussa.web.filter.SessionMonitorFilter.
  */
-public class WebRedirectorServlet extends HttpServlet {
+public class SavedUrlServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		if (req.getSession().getAttribute(SessionMonitorFilter.SAVED_URI_KEY) != null) {
+		if (req.getSession().getAttribute(SavedUrlFilter.SAVED_URI_KEY) != null) {
 			// redirect to *original* URL
-			String uri = (String)req.getSession().getAttribute(SessionMonitorFilter.SAVED_URI_KEY);
-			String queryParams = (String)req.getSession().getAttribute(SessionMonitorFilter.SAVED_QUERY_STRING_KEY);
+			String uri = (String)req.getSession().getAttribute(SavedUrlFilter.SAVED_URI_KEY);
+			String queryParams = (String)req.getSession().getAttribute(SavedUrlFilter.SAVED_QUERY_STRING_KEY);
 			
 			StringBuffer url = new StringBuffer(uri);
 			if (StringUtils.isNotBlank(queryParams)) {
 				url.append("?").append(queryParams);
 			}
 			
-			req.getSession().setAttribute(SessionMonitorFilter.SAVED_URI_KEY, null);
-			req.getSession().setAttribute(SessionMonitorFilter.SAVED_QUERY_STRING_KEY, null);
+			req.getSession().setAttribute(SavedUrlFilter.SAVED_URI_KEY, null);
+			req.getSession().setAttribute(SavedUrlFilter.SAVED_QUERY_STRING_KEY, null);
 			
 			resp.sendRedirect(url.toString());
 		} else {
