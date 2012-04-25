2e6d7d466095fd83a6ebeee5bd40a93482f6c154
tstockett <tstockett@cb3c6908-3955-1147-92f4-2d4536eed4be>
2012-04-25 17:35:17 +0000

    changes to test specific features

--- trunk/src/test/globalrescue/cancelAccount.sh
+++ trunk/src/test/globalrescue/cancelAccount.sh
@@ -1,5 +1,5 @@
 curl \
 -d "partner_guid=E872C58A-15C5-AD6F-99DC-3C81565EA71E" \
 -d "action=cancelAccount" \
--d "account_guid=D187910E-92A5-D377-FD7426374E5B2897" \
+-d "account_guid=E5F570E4-0D6E-EEEF-1C9DFE02E70CA917" \
 http://staging.globalrescue.com/api/index.cfm
