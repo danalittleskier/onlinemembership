02ca8359b53a22257efa8f92804dff1a5830a416
dalexandrescu <dalexandrescu@cb3c6908-3955-1147-92f4-2d4536eed4be>
2014-03-31 15:29:18 +0000

    Adding new memberships Volunteer and Non-Competing

--- trunk/src/main/java/org/ussa/model/Inventory.java
+++ trunk/src/main/java/org/ussa/model/Inventory.java
@@ -61,6 +61,8 @@ public class Inventory implements Serializable
 	public static final String INV_ID_ALPINE_STUDENT = "AS";
 	public static final String INV_ID_ALPINE_OFFICIAL = "AO";
 	public static final String INV_ID_ALPINE_YOUTH = "AY";
+	public static final String INV_ID_ALPINE_VOLUNTEER = "AV";
+	public static final String INV_ID_ALPINE_NON_COMPETING = "AN";
 
 	// Cross country memberships
 	public static final String INV_ID_CROSS_COUNTRY_ASSOCIATE = "XA";
@@ -68,6 +70,8 @@ public class Inventory implements Serializable
 	public static final String INV_ID_CROSS_COUNTRY_COMPETITOR = "XC";
 	public static final String INV_ID_CROSS_COUNTRY_YOUTH = "XY";
 	public static final String INV_ID_CROSS_COUNTRY_OFFICIAL = "XO";
+	public static final String INV_ID_CROSS_COUNTRY_VOLUNTEER = "XV";
+	public static final String INV_ID_CROSS_COUNTRY_NON_COMPETING = "XN";
 
 	// Disabled Alpine memberships
 	public static final String INV_ID_DISABLED_ALPINE_ASSOCIATE = "DAA";
@@ -85,6 +89,8 @@ public class Inventory implements Serializable
 	public static final String INV_ID_FREESTYLE_ROOKIE = "FR";
 	public static final String INV_ID_FREESTYLE_YOUTH = "FY";
 	public static final String INV_ID_FREESKIING_COMPETITOR = "FSC";
+	public static final String INV_ID_FREESTYLE_VOLUNTEER ="FV";
+	public static final String INV_ID_FREESTYLE_NON_COMPETING = "FN";
 
 	// Jumping / Nordic Combined memberships
 	public static final String INV_ID_JUMPING_ASSOCIATE = "JA";
@@ -92,6 +98,8 @@ public class Inventory implements Serializable
 	public static final String INV_ID_JUMPING_COMPETITOR = "JC";
 	public static final String INV_ID_JUMPING_YOUTH = "JY";
 	public static final String INV_ID_JUMPING_OFFICIAL = "JO";
+	public static final String INV_ID_JUMPING_VOLUNTEER = "JV";
+	public static final String INV_ID_JUMPING_NON_COMPETING ="JN";
 
 	// Snowboarding memberships
 	public static final String INV_ID_SNOWBOARD_ASSOCIATE = "BA";
@@ -99,6 +107,8 @@ public class Inventory implements Serializable
 	public static final String INV_ID_SNOWBOARD_COMPETITOR = "BC";
 	public static final String INV_ID_SNOWBOARD_COMPETITOR_REGIONAL = "BCR";
 	public static final String INV_ID_SNOWBOARD_OFFICIAL = "BO";
+	public static final String INV_ID_SNOWBOARD_VOLUNTEER = "BV";
+	public static final String INV_ID_SNOWBOARD_NON_COMPETING = "BN";
 
 	// FIS
 	public static final String INV_ID_ALPINE_FIS = "FISA";
