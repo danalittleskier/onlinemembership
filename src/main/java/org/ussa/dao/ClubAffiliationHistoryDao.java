a0c1a6f7820f2a77b4dc03fab2c8e4a76f10f501
dalexandrescu <dalexandrescu@cb3c6908-3955-1147-92f4-2d4536eed4be>
2014-05-19 17:40:22 +0000

    New dao to keep track of club affiliation

---
+++ src/main/java/org/ussa/dao/ClubAffiliationHistoryDao.java
@@ -0,0 +1,9 @@
+package org.ussa.dao;
+
+import org.ussa.common.dao.GenericDao;
+import org.ussa.model.ClubAffiliationHistory;
+
+public interface ClubAffiliationHistoryDao extends GenericDao<ClubAffiliationHistory, Long>
+{
+
+}
